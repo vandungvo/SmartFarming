90a97796181c87f537b74d000bdb514a12802791
Võ Văn Dũng <dung.vovandung@hcmut.edu.vn>
2024-06-11 13:46:59 +0700

    update

--- main.py
+++ main.py
@@ -1,22 +1,41 @@
-import Utilities.controller
+import sys
 from Adafruit_IO import MQTTClient
-import Utilities.connect
 import Utilities.controller
-import time
-import Utilities.modbus485
+
+AIO_FEED_IDs = ["nutnhan1", "nutnhan2"]
+AIO_USERNAME = "vovandung"
+AIO_KEY = "aio_koLB86LDvN7AIgY9VRbrEU551Ik5"
 
 controller = Utilities.controller.Controller()
 
-client = MQTTClient(Utilities.connect.AIO_USERNAME , Utilities.connect.AIO_KEY)
-client.on_connect = Utilities.connect.Connection.connected
-client.on_subscribe = Utilities.connect.Connection.subscribe
-client.on_disconnect = Utilities.connect.Connection.disconnected
-client.on_message = Utilities.connect.Connection.message
+def connected(client):
+    print("Ket noi thanh cong ...")
+    for topic in AIO_FEED_IDs:
+        client.subscribe(topic)
 
+def subscribe(client , userdata , mid , granted_qos):
+    print("Subscribe thanh cong ...")
+
+def disconnected(client):
+    print("Ngat ket noi ...")
+    sys.exit (1)
+
+def message(client , feed_id , payload):
+    print("Nhan du lieu: " + payload + " , feed id:" + feed_id)
+    if feed_id == "nutnhan1":
+        if payload == "0":
+            controller.controlMixer1("OFF")
+        else:
+            controller.controlMixer1("ON")
+    if feed_id == "nutnhan2":
+        if payload == "0":
+            controller.controlMixer2("OFF")
+        else:
+            controller.controlMixer2("ON")
+
+client = MQTTClient(AIO_USERNAME , AIO_KEY)
+client.on_connect = connected
+client.on_disconnect = disconnected
+client.on_message = message
+client.on_subscribe = subscribe
 client.connect()
-client.loop_background()
-
-while True:
-    controller.readTemperature()
-    controller.readHumidity()
-    time.sleep(1)
